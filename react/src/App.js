caf3ef2d1879a1f9a6bf4ca86c9750d057bb657e
mmpyw01 <69138885+mmpyw01@users.noreply.github.com>
2023-03-13 00:42:35 +0700

    react

---
+++ react/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Menu from './components/Menu'
+
+function App() {
+  return (
+   <>
+   <div className="container mt-5">
+    <Header/>
+    <Menu/>
+    </div>
+   </>
+  );
+}
+
+export default App;
